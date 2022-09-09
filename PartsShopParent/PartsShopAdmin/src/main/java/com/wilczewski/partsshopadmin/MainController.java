cf3e3915d9682e47a33aabab858c873add3d1b3d
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2022-09-09 08:53:38 +0200

    Admin panel - user management

--- PartsShopParent/PartsShopAdmin/src/main/java/com/wilczewski/partsshopadmin/MainController.java
+++ PartsShopParent/PartsShopAdmin/src/main/java/com/wilczewski/partsshopadmin/MainController.java
@@ -1,5 +1,8 @@
 package com.wilczewski.partsshopadmin;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,5 +11,15 @@ public class MainController {
     @GetMapping("")
     public String view(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String viewLoginPage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "login";
+        }
+
+        return "redirect:/";
     }
 }
--- PartsShopParent/PartsShopAdmin/src/main/java/com/wilczewski/partsshopadmin/PartsShopAdminApplication.java
+++ PartsShopParent/PartsShopAdmin/src/main/java/com/wilczewski/partsshopadmin/PartsShopAdminApplication.java
@@ -2,8 +2,10 @@ package com.wilczewski.partsshopadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.wilczewski.partsshopcommon.entity"})
 public class PartsShopAdminApplication {
 
     public static void main(String[] args) {
